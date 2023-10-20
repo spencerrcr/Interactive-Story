9ad01c3554ba42bdb0641405b2a638f909d20bdb
Spencer Recor <spencerrcr@gmail.com>
2023-10-20 05:44:25 -0500

    made changes to page, each section now opens new page, also cleaned up code, and got buttons working

--- scriptOne.js
+++ scriptOne.js
@@ -1,25 +1,25 @@
-function toggleImages(){
-let medMain = document.getElementById('medMain');
-let romanMain = document.getElementById('romanMain');
-let sixtiesMain = document.getElementById('sixtiesMain');
+// function toggleImages(){
+// let medMain = document.getElementById('medMain');
+// let romanMain = document.getElementById('romanMain');
+// let sixtiesMain = document.getElementById('sixtiesMain');
 
-if(getComputedStyle(medMain).display !== 'none'){
-    romanMain.style.display ='none';
-    sixtiesMain.style.display = 'none';
-    medMain.style.display = 'none';
+// if(getComputedStyle(medMain).display !== 'none'){
+//     romanMain.style.display ='none';
+//     sixtiesMain.style.display = 'none';
+//     medMain.style.display = 'none';
 
-    //using ID's show the first two medival selections
-    medhiddingFirstChoice.style.display = 'flex';
-    medhiddingSecondChoice.style.display = 'flex';
-    //using ID's hide the buttons
-    romanHide.style.display = 'none';
-    medHide.style.display ='none';
-    sixtiesHide.style.display= 'none';
-    buttonSection.style.display = 'none';
-} 
-else {
-    medMain.style.display ='block';
-    sixtiesMain.style.display = 'block';
-    romanMain.style.display = 'block';
-}
-}
+//     //using ID's show the first two medival selections
+//     medhiddingFirstChoice.style.display = 'flex';
+//     medhiddingSecondChoice.style.display = 'flex';
+//     //using ID's hide the buttons
+//     romanHide.style.display = 'none';
+//     medHide.style.display ='none';
+//     sixtiesHide.style.display= 'none';
+//     buttonSection.style.display = 'none';
+// } 
+// else {
+//     medMain.style.display ='block';
+//     sixtiesMain.style.display = 'block';
+//     romanMain.style.display = 'block';
+// }
+// }
